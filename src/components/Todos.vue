c49090fd200c20b7a341ec77bdd274acc652f086
nutdanai <nutdanai@github.com>
2018-01-31 20:02:58 +0700

    1-7

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,7 +2,7 @@
   <div>
     <div v-for="(todo , index) in todos" :key="todo.title" v-if="checkTodo(todo)">
       <b-field class="is-pulled-left">
-        <b-checkbox size="is-large" @input=checkbox(index)>
+        <b-checkbox size="is-large" v-model="todo.complete" @input="checkbox(index)">
           <strike v-if="todo.completed">{{ todo.title }}</strike>
           <span v-else>{{ todo.title }}</span>
         </b-checkbox>
